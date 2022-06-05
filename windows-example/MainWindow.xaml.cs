aa8a9b31945ffb73f3ba48218c54fe19dbfc5ba3
DUJIEPENG\dujiepeng <347302029@qq.com>
2022-06-05 11:23:09 +0800

    1. 添加关闭窗口回调； 2. 删除代码实现；

--- windows-example/MainWindow.xaml.cs
+++ windows-example/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ namespace windows_example
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window, IChatManagerDelegate
+    public partial class MainWindow : Window
     {
+        // 初始化SDK用的Appkey，此处的值为演示用，如果正式环境需要使用你申请的Appkey
         private static readonly string APPKEY = "41117440#383391";
 
         private readonly System.Windows.Threading.Dispatcher Dip = null;
@@ -22,22 +23,38 @@ namespace windows_example
         public MainWindow()
         {
             InitializeComponent();
+            Closed += CloseWindow;
             Dip = System.Windows.Threading.Dispatcher.CurrentDispatcher;
             InitSDK();
             AddChatDelegate();
         }
 
+        // 初始化聊天SDK
         private void InitSDK()
         {
-            Options options = new Options(appKey: APPKEY);
-            SDKClient.Instance.InitWithOptions(options);
+
         }
 
+        // 添加消息监听
         private void AddChatDelegate()
         {
-            SDKClient.Instance.ChatManager.AddChatManagerDelegate(this);
+
         }
 
+        // 移除消息监听
+        private void RemoveChatDelegate()
+        {
+
+        }
+
+
+        // 关闭窗口事件
+        private void CloseWindow(object sender, EventArgs e)
+        {
+            RemoveChatDelegate();
+        }
+
+        // 点击SignIn按钮
         private async void SignIn_Click(object sender, RoutedEventArgs e)
         {
             if (UserIdTextBox.Text.Length == 0 || PasswordTextBox.Text.Length == 0)
@@ -50,16 +67,7 @@ namespace windows_example
             string token = await LoginToAppServer(UserIdTextBox.Text, PasswordTextBox.Text);
             if (token != null)
             {
-                SDKClient.Instance.LoginWithAgoraToken(UserIdTextBox.Text, token, handle: new CallBack(
-                    onSuccess: () =>
-                    {
-                        AddLogToLogText("sign in sdk succeed");
-                    },
-                    onError: (code, desc) =>
-                    {
-                        AddLogToLogText($"sign in sdk failed, code: {code}, desc: {desc}");
-                    }
-                ));
+                // TODO：
             }
             else
             {
@@ -67,6 +75,7 @@ namespace windows_example
             }
         }
 
+        // 点击SignUp按钮
         private async void SignUp_Click(object sender, RoutedEventArgs e)
         {
             if (UserIdTextBox.Text.Length == 0 || PasswordTextBox.Text.Length == 0)
@@ -87,20 +96,13 @@ namespace windows_example
 
         }
 
+        // 点击SignOut按钮
         private void SignOut_Click(object sender, RoutedEventArgs e)
         {
-            SDKClient.Instance.Logout(true, handle: new CallBack(
-               onSuccess: () =>
-               {
-                   AddLogToLogText("sign out sdk succeed");
-               },
-               onError: (code, desc) =>
-               {
-                   AddLogToLogText($"sign out sdk failed, code: {code}, desc: {desc}");
-               }
-           ));
+            // TODO:
         }
 
+        // 点击Send按钮
         private void SendBtn_Click(object sender, RoutedEventArgs e)
         {
             if (SingleChatIdTextBox.Text.Length == 0)
@@ -114,23 +116,12 @@ namespace windows_example
                 AddLogToLogText("message content is null !");
                 return;
             }
-            Message msg = Message.CreateTextSendMessage(SingleChatIdTextBox.Text, MessageContentTextBox.Text);
-            SDKClient.Instance.ChatManager.SendMessage(ref msg, new CallBack(
-                onSuccess: () =>
-                {
-                    Dip.Invoke(() =>
-                    {
-                        AddLogToLogText($"send message succeed, receiver: {SingleChatIdTextBox.Text},  message: {MessageContentTextBox.Text}");
-                    });
-                },
-                onError: (code, desc) =>
-                {
-                    AddLogToLogText($"send message failed, code: {code}, desc: {desc}");
-                }
-            ));
+            
+            // TODO:
 
         }
 
+        // 添加日志到控制台
         private void AddLogToLogText(string log)
         {
             Dip.Invoke(() =>
@@ -140,44 +131,7 @@ namespace windows_example
             });
         }
 
-        public void OnMessagesReceived(List<Message> messages)
-        {
-            foreach (Message msg in messages)
-            {
-                if (msg.Body.Type == MessageBodyType.TXT)
-                {
-                    TextBody txtBody = msg.Body as TextBody;
-                    AddLogToLogText($"received text message: {txtBody.Text}, from: {msg.From}");
-                }
-                else if (msg.Body.Type == MessageBodyType.IMAGE)
-                {
-                    _ = msg.Body as ImageBody;
-                    AddLogToLogText($"received image message, from: {msg.From}");
-                }
-                else if (msg.Body.Type == MessageBodyType.VIDEO)
-                {
-                    _ = msg.Body as VideoBody;
-                    AddLogToLogText($"received video message, from: {msg.From}");
-                }
-                else if (msg.Body.Type == MessageBodyType.VOICE)
-                {
-                    _ = msg.Body as VoiceBody;
-                    AddLogToLogText($"received voice message, from: {msg.From}");
-                }
-                else if (msg.Body.Type == MessageBodyType.LOCATION)
-                {
-                    _ = msg.Body as LocationBody;
-                    AddLogToLogText($"received location message, from: {msg.From}");
-                }
-                else if (msg.Body.Type == MessageBodyType.FILE)
-                {
-                    _ = msg.Body as FileBody;
-                    AddLogToLogText($"received file message, from: {msg.From}");
-                }
-            }
-        }
-
-
+        // 根据账号密码获取登录token
         private async Task<string> LoginToAppServer(string username, string password)
         {
             Dictionary<string, string> values = new Dictionary<string, string>();
@@ -200,6 +154,7 @@ namespace windows_example
             }
         }
 
+        // 注册账号
         private async Task<Boolean> RegisterToAppServer(string username, string password)
         {
     
@@ -229,44 +184,5 @@ namespace windows_example
             }
         }
 
-        public void OnCmdMessagesReceived(List<Message> messages)
-        {
-
-        }
-
-        public void OnMessagesRead(List<Message> messages)
-        {
-
-        }
-
-        public void OnMessagesDelivered(List<Message> messages)
-        {
-
-        }
-
-        public void OnMessagesRecalled(List<Message> messages)
-        {
-
-        }
-
-        public void OnReadAckForGroupMessageUpdated()
-        {
-
-        }
-
-        public void OnGroupMessageRead(List<GroupReadAck> list)
-        {
-
-        }
-
-        public void OnConversationsUpdate()
-        {
-
-        }
-
-        public void OnConversationRead(string from, string to)
-        {
-
-        }
     }
 }
